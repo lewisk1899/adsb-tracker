d97053baa7265c302c521ca11295df9c94e2fbe3
Lewis <lwk1899@gmail.com>
2025-05-15 03:41:05 +0000

    Linted code

--- backend/get_adsb_data.py
+++ backend/get_adsb_data.py
@@ -1,29 +1,28 @@
-import requests
-from datetime import datetime, timezone
+""" Pulls Data From OpenSky """
+import json
 import os
 import time
-import json
-
-from loguru import logger
-from models.state import States
+from datetime import datetime, timezone
 from typing import Optional
+
+import requests
+from models.state import States
 
 OUTPUT_DIRECTORY = "/data/opensky/"
 
-def poll_open_sky(url: str = "https://opensky-network.org/", 
-                  endpoint: str = "api/states/all"
+
+def poll_open_sky(
+    url: str = "https://opensky-network.org/", endpoint: str = "api/states/all"
 ) -> Optional[States]:
     """ Grabs data from OpenSky Live API """
     try:
         response = requests.get(url + endpoint, timeout=5)
-    except requests.exceptions.RequestException as err:
-        print("OOps: Something Else",err)
     except requests.exceptions.HTTPError as errh:
-        print("Http Error:",errh)
+        print("Http Error:", errh)
     except requests.exceptions.ConnectionError as errc:
-        print("Error Connecting:",errc)
+        print("Error Connecting:", errc)
     except requests.exceptions.Timeout as errt:
-        print("Timeout Error:",errt) 
+        print("Timeout Error:", errt)
 
     if response.status_code == 200:
         data = response.json()
@@ -31,27 +30,31 @@ def poll_open_sky(url: str = "https://opensky-network.org/",
         return states
 
     return None
-    
+
 
 def states_to_file(states: States, output_path: str):
     """ Write States to file nambed by the time it was pulled """
     utc_string = states.time
-    zulu_time = datetime.fromtimestamp(utc_string, tz=timezone.utc).strftime("%Y-%m-%d %H:%M:%S Z")
+    zulu_time = datetime.fromtimestamp(utc_string, tz=timezone.utc).strftime(
+        "%Y-%m-%d %H:%M:%S Z"
+    )
 
     fileout_path = output_path + str(zulu_time) + ".json"
-    with open(fileout_path, "w") as fout:
+
+    with open(fileout_path, "w", encoding="utf-8") as fout:
         json.dump(states.model_dump(), fout, indent=4)
-    
-     
+
+
 def main():
+    """ Testing if this works """
     if not os.path.exists(OUTPUT_DIRECTORY):
-       os.mkdir(OUTPUT_DIRECTORY)
-    
+        os.mkdir(OUTPUT_DIRECTORY)
+
     while True:
         states = poll_open_sky()
         states_to_file(states, OUTPUT_DIRECTORY)
         time.sleep(600)
-    
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- backend/models/state.py
+++ backend/models/state.py
@@ -1,7 +1,11 @@
-from pydantic import BaseModel, root_validator
+""" Pydantic Validation For OpenSky Response """
 from typing import List, Optional
 
+from pydantic import BaseModel, root_validator
+
+
 class StateVector(BaseModel):
+    """ OpenSky StateVector Validation """
     icao24: str
     callsign: Optional[str]
     origin_country: str
@@ -22,20 +26,38 @@ class StateVector(BaseModel):
 
     @classmethod
     def from_list(cls, data: List):
+        """ Converting Data From List to Pydantic Type """
         return cls(
-            icao24=data[0], callsign=data[1], origin_country=data[2], time_position=data[3],
-            last_contact=data[4], longitude=data[5], latitude=data[6], baro_altitude=data[7],
-            on_ground=data[8], velocity=data[9], true_track=data[10], vertical_rate=data[11],
-            sensors=data[12], geo_altitude=data[13], squawk=data[14], spi=data[15], position_source=data[16]
+            icao24=data[0],
+            callsign=data[1],
+            origin_country=data[2],
+            time_position=data[3],
+            last_contact=data[4],
+            longitude=data[5],
+            latitude=data[6],
+            baro_altitude=data[7],
+            on_ground=data[8],
+            velocity=data[9],
+            true_track=data[10],
+            vertical_rate=data[11],
+            sensors=data[12],
+            geo_altitude=data[13],
+            squawk=data[14],
+            spi=data[15],
+            position_source=data[16],
         )
 
+
 class States(BaseModel):
+    """ States Validation """
     time: int
     states: List[StateVector]
 
     @root_validator(pre=True)
-    def parse_states(cls, values):
+    def parse_states(self, values):
+        """ Parsing States """
         if "states" in values and isinstance(values["states"], list):
-            values["states"] = [StateVector.from_list(state) for state in values["states"]]
+            values["states"] = [
+                StateVector.from_list(state) for state in values["states"]
+            ]
         return values
-
